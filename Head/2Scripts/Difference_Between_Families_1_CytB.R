19de3f37a844e1eee760a0c02778635238862640
Dmitrii Iliushchenko <seroussamkrsk@gmail.com>
2020-03-17 12:59:47 +0200

    Update Difference_Between_Families_1_CytB.R

--- Head/2Scripts/Difference_Between_Families_1_CytB.R
+++ Head/2Scripts/Difference_Between_Families_1_CytB.R
@@ -1,9 +1,9 @@
 library(Biostrings)
 library(seqinr)
-
+setwd('C:/USers/Professional/Desktop')
 rm(list=ls(all=TRUE))
 
-#setwd('C:/Users/Professional/Desktop')
+
 wd = getwd()
 wd = paste(wd, '/mtdna-mammalian-evolution/Body/1Raw',sep='')
 setwd(wd)
@@ -19,9 +19,16 @@ GenerationL$Scientific_name = gsub(' ','_',GenerationL$Scientific_name)
 
 GenFam = merge(GenerationL, families, by.x = 'Scientific_name', by.y = 'Species' ) 
 GenFam = GenFam[complete.cases(GenFam), ] #### remove rows with NA
+GenFam = GenFam[-which(duplicated(GenFam$Scientific_name)),] ### delete repeated species
 
-#### get just 2 families 
-GenFam = GenFam[GenFam$ECO.Family == c('Cervidae','Dasypodidae'),]
+##### remove families with sppecies < 3 ######
+VecOfFamilies = unique(GenFam$ECO.Family)
+for (i in VecOfFamilies) {
+  b = nrow(GenFam[GenFam$ECO.Family == i,])
+  if ( b < 3){
+    GenFam = GenFam[GenFam$ECO.Family != i,]
+  }
+}
 
 VecOfFamilies = unique(GenFam$ECO.Family)### new vector for families with species >= 3
 VecOfFamilies = as.character(VecOfFamilies)
@@ -30,26 +37,28 @@ VecOfFamilies = as.character(VecOfFamilies)
 wd = paste(wd, '/fasta_codons', sep='')
 setwd(wd) ### Change direcotry for Codons
 
-DifferenceBetweenSpecies = data.frame()
 
-numofloop = 0
-
-files = list.files(pattern=".*\\.fasta")
- 
-
-  
+###### read file with sequence CytB #######
 Testdf = readDNAStringSet('CytB.fasta')
 Testdf = data.frame(Testdf); names(Testdf) = c('Sequence') 
 Testdf$Scientific_name  = as.factor(rownames(Testdf))
 row.names(Testdf) = c(1:nrow(Testdf))
-  
-  
+
+#### merge dataframe GenLen with dataframe Sequence
 MergeDf = merge(GenFam, Testdf, by = 'Scientific_name' )
-  
-  
+
+wd = gsub('1Raw/fasta_codons','2Derived/DifferenceFamiliesCytB',wd) # change directory for our output 
+setwd(wd)
+
+
 ####### Create a new data frame with 3 species: High and Low Generation time, and Outgroup####
-for (fam in VecOfFamilies){
-  ##fam = 'Cervidae'
+for (fam in VecOfFamilies) {
+  #fam = 'Felidae'
+  
+  numofloop = 1
+  DifferenceInOneFamily = data.frame()
+  
+  
   OneFamilyDf = MergeDf[MergeDf$ECO.Family == fam,]
   NumberOfSp = nrow(OneFamilyDf)
   
@@ -58,7 +67,7 @@ for (fam in VecOfFamilies){
     Species1 = OneFamilyDf[sp1,]
     
     for (sp2 in (sp1+1):(NumberOfSp-1)){
-      #sp2 = 2
+      #sp = 2
       Species2 = OneFamilyDf[sp2,]
       
       for(sp3 in (sp2+1):NumberOfSp){
@@ -73,21 +82,22 @@ for (fam in VecOfFamilies){
         VecOfNuc3 = unlist(strsplit(Species3$Sequence,''))
         
         StartNuc = 1
+        
         Codons1 = c()
         Codons2 = c()
         Codons3 = c()
         
         ######## Create vectors for differences ##################
         
-        DifBtwSp1AndSp2 = c()
-        DifBtwSp1AndSp3 = c()
-        DifBtwSp2AndSp3 = c()
-          
+        DifBtwSp1AndSp2 = ''
+        DifBtwSp1AndSp3 = ''
+        DifBtwSp2AndSp3 = ''
+        
         ############# Create a new data frame with information about Species and  difference between every species for One Gene ##############################
         
-        DifferenceInOneGene = data.frame(Species1$Scientific_name, Species2$Scientific_name, Species3$Scientific_name, Species1$ECO.Family, Species1$GenerationLength_d,
+        DifferenceInOneTrio = data.frame(Species1$Scientific_name, Species2$Scientific_name, Species3$Scientific_name, Species1$ECO.Family, Species1$GenerationLength_d,
                                          Species2$GenerationLength_d, Species3$GenerationLength_d, '', '', '', 'CytB') 
-        names(DifferenceInOneGene) = c('Species1', 'Species2', 'Species3', 'Family', 'GenLenSp1', 'GenLenSp2','GenLenSp3', 'AllCodonSubstBetweenSp1AndSp2' ,
+        names(DifferenceInOneTrio) = c('Species1', 'Species2', 'Species3', 'Family', 'GenLenSp1', 'GenLenSp2','GenLenSp3', 'AllCodonSubstBetweenSp1AndSp2' ,
                                        'AllCodonSubstBetweenSp1AndSp3', 'AllCodonSubstBetweenSp2AndSp3', 'Gene')
         
         
@@ -100,15 +110,15 @@ for (fam in VecOfFamilies){
           Codons1 = paste(Codons1, collapse = '')
           Codons2 = paste(Codons2, collapse = '')
           if (Codons1 != Codons2){
-            DifBtwSp1AndSp2 = paste(DifBtwSp1AndSp2, StartNuc,':', Codons1, '>', Codons2, ';', sep = "")
+            DifBtwSp1AndSp2 = paste(DifBtwSp1AndSp2,StartNuc, ':', Codons1, '>', Codons2, ';', sep = "")
           }
           StartNuc = StartNuc + 3
         }
-          
-        DifferenceInOneGene$AllCodonSubstBetweenSp1AndSp2 = DifBtwSp1AndSp2
-          
+        
+        DifferenceInOneTrio$AllCodonSubstBetweenSp1AndSp2 = DifBtwSp1AndSp2
+        
         StartNuc = 1
-          
+        
         for (i in 1:length(VecOfNuc1)/3){
           ##### i = 1
           Codons1 = c(VecOfNuc1[StartNuc:(StartNuc+2)])
@@ -116,44 +126,44 @@ for (fam in VecOfFamilies){
           Codons1 = paste(Codons1, collapse = '')
           Codons3 = paste(Codons3, collapse = '')
           if (Codons1 != Codons3){
-            DifBtwSp1AndSp3 = paste(DifBtwSp1AndSp3, StartNuc,':', Codons1, '>', Codons3, ';', sep = "")
+            DifBtwSp1AndSp3 = paste(DifBtwSp1AndSp3, StartNuc, ':', Codons1, '>', Codons3, ';', sep = "")
           }
           StartNuc = StartNuc + 3
         }
         
-        DifferenceInOneGene$AllCodonSubstBetweenSp1AndSp3 = DifBtwSp1AndSp3
+        DifferenceInOneTrio$AllCodonSubstBetweenSp1AndSp3 = DifBtwSp1AndSp3
         
         StartNuc = 1
+        
         for (i in 1:length(VecOfNuc2)/3){
           ##### i = 1
           Codons2 = c(VecOfNuc2[StartNuc:(StartNuc+2)])
           Codons3 = c(VecOfNuc3[StartNuc:(StartNuc+2)])
           Codons2 = paste(Codons2, collapse = '')
           Codons3 = paste(Codons3, collapse = '')
-          if (Codons2 != Codons3){
-            DifBtwSp2AndSp3 = paste(DifBtwSp2AndSp3, StartNuc,':', Codons2, '>', Codons3, ';', sep = "")
-          }
+          if (Codons2 != Codons3){ DifBtwSp2AndSp3 = paste(DifBtwSp2AndSp3, StartNuc, ':', Codons2, '>', Codons3, ';', sep = "")}
           StartNuc = StartNuc + 3
         }
-          
-        DifferenceInOneGene$AllCodonSubstBetweenSp2AndSp3 = DifBtwSp2AndSp3
+        
+        
+        DifferenceInOneTrio$AllCodonSubstBetweenSp2AndSp3 = DifBtwSp2AndSp3
+      
+        if (numofloop == 1) {
+          DifferenceInOneFamily = DifferenceInOneTrio
+        } else {
+          DifferenceInOneFamily = rbind(DifferenceInOneFamily,DifferenceInOneTrio)
+        }
         
         numofloop = numofloop + 1
-        
-        if (numofloop == 1){
-          DifferenceBetweenSpecies = DifferenceInOneGene
-        } else {
-          DifferenceBetweenSpecies = rbind(DifferenceBetweenSpecies, DifferenceInOneGene)
-        }
       }
     }
   }
+  
+  #### write file for a family in CytB #####
+  filename = paste('DifferenceBetween', fam ,'.txt', sep = '')
+  write.table(DifferenceInOneFamily,file = filename)
+
 }
 
 
-
-
-wd = gsub('/1Raw/fasta_codons', '/2Derived', wd)
-setwd(wd)
-write.table(DifferenceBetweenSpecies, file = "DifferenceBetweenSpeciesFamilies1CytB.txt")
 
