79d4250685a75a29a7806548b7c9e6b05600e4ff
Anastasia <anastasia3sokol@yandex.ru>
2020-07-30 16:21:13 +0200

    Script that counts number of substitutions and Grantham distance

--- Head/2Scripts/02.CodonChangeAnnotation.R
+++ Head/2Scripts/02.CodonChangeAnnotation.R
@@ -7,14 +7,14 @@ rm(list=ls(all=TRUE))
 
 library(seqinr)
 library(Biostrings)
+library(dplyr)
+library(RGenetics)
 SGC1 <- getGeneticCode("SGC1")  # Vertebrate Mitochondrial code for translate function
 
 
 #### read data:
-Codons = read.table("../../Body/2Derived/PolymorphicPairwiseCodons.txt")
-dim(Codons)
-
-#  "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
+Codons = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/PolymorphicPairwiseCodons.txt") 
+Codons2 <- Codons[1:1000,]
 
 ### functions which take example line as input and return different metrics: 
 # number of all codon substitutions,
@@ -22,75 +22,101 @@ dim(Codons)
 # number of nonsyn substitutions, 
 # within nonsyn substitutions - average grantham distance between the first and the second AA
 
-#### 1 - count number of all codon changes
-# x = Codons$SubstVec[1] 
-# "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
 
 TotalDiv <- function(x) {Div = length(unlist(strsplit(x,';')))}
-Codons$TotalDiv = apply(as.matrix(Codons$SubstVec),1,FUN = TotalDiv)
+Codons$TotalDiv <- apply(as.matrix(Codons$SubstVec),1,FUN = TotalDiv)
 summary(Codons$TotalDiv)
 
-#### 2 - count number of syn and nons changes
-# x = Codons$SubstVec[1] 
-# "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
 
-NumberSynNons <- function(x) 
+
+data(aaindex)
+names(aaindex) # 544 codes related to different amino-acid properties
+aaindex$GRAR740101 # composition from Grantham 
+aaindex$GRAR740102 # polarity from Grantham 
+aaindex$GRAR740103 # volume from Grantham 
+
+GranthamDistance = data.frame(cbind(aaindex$GRAR740101$I,aaindex$GRAR740102$I,aaindex$GRAR740103$I))
+names(GranthamDistance)=c("Composition","Polarity","Volume")
+GranthamDistance$Aaa = row.names(GranthamDistance)
+GranthamDistance$A = a(GranthamDistance$Aaa)
+str(GranthamDistance)
+
+Synon <- 0
+Non_Synon <- 0
+Composition <- ''
+Polarity<- ""
+Volume <- ""
+Grantham <- ""
+
+###########333
+
+one_line = c()
+
+#x = "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
+#x = "30:GAA>GNA;200:AAT>AAC;321:CCC>CCT;"
+Translation <- function(x) 
 {
+  
   VecOfCodons = unlist(strsplit(x,';'));
-  for (i in 1:length(VecOfCodons))
-  { # i = 1
-    CodonSubst = VecOfCodons[i]
+  
+  AminoSubs=c();
+  Output <- c()
+  Grantham = c()
+  VecOfDistances = c()
+  Indel <- 0
+  N <- 0
+  Check <- c("AAA","AAT","AAC","AAG","ATA","ATT","ATC","ATG","ACA","ACT","ACC","ACG","AGA","AGT","AGC","AGG","TAA","TAT","TAC","TAG","TTA","TTT","TTC","TTG","TCA","TCT","TCC","TCG","TGA","TGT","TGC","TGG","CAA","CAT","CAC","CAG","CTA","CTT","CTC","CTG","CCA","CCT","CCC","CCG","CGA","CGT","CGC","CGG","GAA","GAT","GAC","GAG","GTA","GTT","GTC","GTG","GCA","GCT","GCC","GCG","GGA","GGT","GGC","GGG")
+  for (j in (1:length(VecOfCodons))) 
+  {
+    ##j =11
+    CodonSubst = VecOfCodons[j]
     CodonSubst = gsub("(.*)\\:",'',CodonSubst)
     CodonSubst1 = gsub(">(.*)",'',CodonSubst)
     CodonSubst2 = gsub("(.*)>",'',CodonSubst)
     
-    Codon1 <- DNAString(CodonSubst1)
-    Codon2  <- DNAString(CodonSubst2)
-    Codon1.Character = as.character(Codon1)
-    Codon2.Character = as.character(Codon2)
-    if  (grepl('-',Codon1.Character) | grepl('-',Codon2.Character)) {Indel=Indel+1; break}
-    if  (!grepl('-',Codon1.Character) & !grepl('-',Codon2.Character)) 
-    {
+    if (CodonSubst1 %in% Check & CodonSubst2 %in% Check ) {
+      
+      Codon1 <- DNAString(CodonSubst1)
+      Codon2  <- DNAString(CodonSubst2)
+      
+      Codon1.Character = as.character(Codon1)
+      Codon2.Character = as.character(Codon2)
+      #if  (grepl('-',Codon1.Character) | grepl('-',Codon2.Character)) {Indel=Indel+1; break}
+      #if  (!grepl('-',Codon1.Character) & !grepl('-',Codon2.Character)) 
+      
+      #{ 
       A1 = as.character(Biostrings::translate(Codon1, genetic.code=SGC1))
       A2 = as.character(Biostrings::translate(Codon2, genetic.code=SGC1))
-      if (A1 == A2) {Synon=Synon+1}
-    }
-    if  (A1 != A2) 
       
-    
-return  paste(number of Syn subst, number of Nons subst, list of all AA subst: C>S,A>V...)
-    
-  "5;3;C>S,A>V,A>V"
-  
-  apply  
-    
-    
-    
-        
-    
+      Subs = paste(A1,'>',A2,sep = '')
+      
+      AminoSubs = c(AminoSubs, Subs)  
+      
+      
+      if (A1 == A2) {Synon <- Synon + 1} else {Non_Synon <- Non_Synon +1}
+      
+      SquareOfDiffInComposition =  (GranthamDistance[GranthamDistance$A == A1,]$Composition - GranthamDistance[GranthamDistance$A == A2,]$Composition)^2
+      # Composition <<- c(Composition,paste(SquareOfDiffInComposition)) 
+      
+      SquareOfDiffInPolarity =    (GranthamDistance[GranthamDistance$A == A1,]$Polarity - GranthamDistance[GranthamDistance$A == A2,]$Polarity)^2
+      # Polarity <<- c(Polarity,paste(SquareOfDiffInPolarity))
+      
+      SquareOfDiffInVolume   =    (GranthamDistance[GranthamDistance$A == A1,]$Volume  - GranthamDistance[GranthamDistance$A == A2,]$Volume)^2
+      # Volume <<- c(Volume,paste(SquareOfDiffInVolume))
+      
+      GranthamNew <- (SquareOfDiffInComposition + SquareOfDiffInPolarity + SquareOfDiffInVolume)^0.5
+      Grantham <<-c(Grantham,paste(GranthamNew))
+      
+      # Output=paste(Synon,Non_Synon,Grantham,sep=';')
+      # one_line = rbind(one_line, c(AminoSubs, Grantham))
+      
+      AminoGrantham = paste(Subs, ':', GranthamNew,';','Synon:', Synon,';','Non_Synon:', Non_Synon, sep='')
+      
+      VecOfDistances = c(VecOfDistances, AminoGrantham)
+      #} 
+    }else{N=N+1;break}
   }
-  
-Div = length(unlist(strsplit(x,';')))
-  
+  return(VecOfDistances)
 }
 
-
-#### from DIMA: TracerA
-
-Codon1 <- DNAString(TwoCodons[1])
-Codon2  <- DNAString(TwoCodons[2])
-Codon1.Character = as.character(Codon1)
-Codon2.Character = as.character(Codon2)
-if  (grepl('-',Codon1.Character) | grepl('-',Codon2.Character)) {Indel=Indel+1; break}
-if  (!grepl('-',Codon1.Character) & !grepl('-',Codon2.Character)) 
-{
-  A1 = as.character(Biostrings::translate(Codon1, genetic.code=SGC1))
-  A2 = as.character(Biostrings::translate(Codon2, genetic.code=SGC1))
-  if (A1 == A2) {Synon=Synon+1}
-}
-if  (A1 != A2) 
-{ # estimate Grantham distance 
-  # from https://en.wikipedia.org/wiki/Amino_acid_replacement:
-  # Grantham's distance depends on 3 p
-
-
+Codons$Distances = lapply(as.character(Codons$SubstVec), Translation)
