ea5c371f7a7cff56a94b7f39bee8fe436836d6d2
Anastasia <anastasia3sokol@yandex.ru>
2020-11-17 07:28:24 +0200

    with median

--- Head/2Scripts/02.CodonChangeAnnotation.R
+++ Head/2Scripts/02.CodonChangeAnnotation.R
@@ -1,122 +1,131 @@
-rm(list=ls(all=TRUE))
-
-#### libraries
-# if (!requireNamespace("BiocManager", quietly = TRUE))
-# install.packages("BiocManager")
-# BiocManager::install("Biostrings")
-
-library(seqinr)
-library(Biostrings)
-library(dplyr)
-library(RGenetics)
-SGC1 <- getGeneticCode("SGC1")  # Vertebrate Mitochondrial code for translate function
-
-
-#### read data:
-Codons = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/PolymorphicPairwiseCodons.txt") 
-Codons2 <- Codons[1:1000,]
-
-### functions which take example line as input and return different metrics: 
-# number of all codon substitutions,
-# number of of syn substitutions,
-# number of nonsyn substitutions, 
-# within nonsyn substitutions - average grantham distance between the first and the second AA
-
-
-TotalDiv <- function(x) {Div = length(unlist(strsplit(x,';')))}
-Codons$TotalDiv <- apply(as.matrix(Codons$SubstVec),1,FUN = TotalDiv)
-summary(Codons$TotalDiv)
-
-
-
-data(aaindex)
-names(aaindex) # 544 codes related to different amino-acid properties
-aaindex$GRAR740101 # composition from Grantham 
-aaindex$GRAR740102 # polarity from Grantham 
-aaindex$GRAR740103 # volume from Grantham 
-
-GranthamDistance = data.frame(cbind(aaindex$GRAR740101$I,aaindex$GRAR740102$I,aaindex$GRAR740103$I))
-names(GranthamDistance)=c("Composition","Polarity","Volume")
-GranthamDistance$Aaa = row.names(GranthamDistance)
-GranthamDistance$A = a(GranthamDistance$Aaa)
-str(GranthamDistance)
-
-Synon <- 0
-Non_Synon <- 0
-Composition <- ''
-Polarity<- ""
-Volume <- ""
-Grantham <- ""
-
-###########333
-
-one_line = c()
-
-#x = "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
-#x = "30:GAA>GNA;200:AAT>AAC;321:CCC>CCT;"
-Translation <- function(x) 
-{
+  rm(list=ls(all=TRUE))
   
-  VecOfCodons = unlist(strsplit(x,';'));
+  #### libraries
+  # if (!requireNamespace("BiocManager", quietly = TRUE))
+  # install.packages("BiocManager")
+  # BiocManager::install("Biostrings")
   
-  AminoSubs=c();
-  Output <- c()
-  Grantham = c()
-  VecOfDistances = c()
-  Indel <- 0
-  N <- 0
-  Check <- c("AAA","AAT","AAC","AAG","ATA","ATT","ATC","ATG","ACA","ACT","ACC","ACG","AGA","AGT","AGC","AGG","TAA","TAT","TAC","TAG","TTA","TTT","TTC","TTG","TCA","TCT","TCC","TCG","TGA","TGT","TGC","TGG","CAA","CAT","CAC","CAG","CTA","CTT","CTC","CTG","CCA","CCT","CCC","CCG","CGA","CGT","CGC","CGG","GAA","GAT","GAC","GAG","GTA","GTT","GTC","GTG","GCA","GCT","GCC","GCG","GGA","GGT","GGC","GGG")
-  for (j in (1:length(VecOfCodons))) 
+  library(seqinr)
+  library(Biostrings)
+  library(dplyr)
+  library(RGenetics)
+  SGC1 <- getGeneticCode("SGC1")  # Vertebrate Mitochondrial code for translate function
+  
+  
+  #### read data:
+  Codons = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/PolymorphicPairwiseCodons.txt") 
+  #Codons <- Codons2[1:100,]
+  
+  ### functions which take example line as input and return different metrics: 
+  # number of all codon substitutions,
+  # number of of syn substitutions,
+  # number of nonsyn substitutions, 
+  # within nonsyn substitutions - average grantham distance between the first and the second AA
+  
+  
+  TotalDiv <- function(x) {Div = length(unlist(strsplit(x,';')))}
+  Codons$TotalDiv <- apply(as.matrix(Codons$SubstVec),1,FUN = TotalDiv)
+  summary(Codons$TotalDiv)
+  
+  
+  
+  data(aaindex)
+  names(aaindex) # 544 codes related to different amino-acid properties
+  aaindex$GRAR740101 # composition from Grantham 
+  aaindex$GRAR740102 # polarity from Grantham 
+  aaindex$GRAR740103 # volume from Grantham 
+  
+  GranthamDistance = data.frame(cbind(aaindex$GRAR740101$I,aaindex$GRAR740102$I,aaindex$GRAR740103$I))
+  names(GranthamDistance)=c("Composition","Polarity","Volume")
+  GranthamDistance$Aaa = row.names(GranthamDistance)
+  GranthamDistance$A = a(GranthamDistance$Aaa)
+  str(GranthamDistance)
+  
+  Synon <- 0
+  Non_Synon <- 0
+  Composition <- ''
+  Polarity<- ""
+  Volume <- ""
+  Grantham <- ""
+  
+  ###########333
+  
+  one_line = c()
+  
+  #x = "3:TGC>AGC;7:ACA>ACC;8:CAT>CAC;9:CCC>CCT;14:GCT>GCG;17:GCG>ACG;26:AAC>AGC;369:GCA>ACC;374:AAC>AAT;377:TTA>ATA;380:GCC>GCT;"
+  #x = "30:GAA>GNA;200:AAT>AAC;321:CCC>CCT;"
+  Translation <- function(x) 
   {
-    ##j =11
-    CodonSubst = VecOfCodons[j]
-    CodonSubst = gsub("(.*)\\:",'',CodonSubst)
-    CodonSubst1 = gsub(">(.*)",'',CodonSubst)
-    CodonSubst2 = gsub("(.*)>",'',CodonSubst)
     
-    if (CodonSubst1 %in% Check & CodonSubst2 %in% Check ) {
+    VecOfCodons = unlist(strsplit(x,';'));
+    
+    AminoSubs=c();
+    Output <- c()
+    Grantham = c()
+    VecOfDistances = c()
+    Indel <- 0
+    N <- 0
+    Check <- c("AAA","AAT","AAC","AAG","ATA","ATT","ATC","ATG","ACA","ACT","ACC","ACG","AGA","AGT","AGC","AGG","TAA","TAT","TAC","TAG","TTA","TTT","TTC","TTG","TCA","TCT","TCC","TCG","TGA","TGT","TGC","TGG","CAA","CAT","CAC","CAG","CTA","CTT","CTC","CTG","CCA","CCT","CCC","CCG","CGA","CGT","CGC","CGG","GAA","GAT","GAC","GAG","GTA","GTT","GTC","GTG","GCA","GCT","GCC","GCG","GGA","GGT","GGC","GGG")
+    for (j in (1:length(VecOfCodons))) 
+    {
+      ##j =11
+      CodonSubst = VecOfCodons[j]
+      CodonSubst = gsub("(.*)\\:",'',CodonSubst)
+      CodonSubst1 = gsub(">(.*)",'',CodonSubst)
+      CodonSubst2 = gsub("(.*)>",'',CodonSubst)
       
-      Codon1 <- DNAString(CodonSubst1)
-      Codon2  <- DNAString(CodonSubst2)
-      
-      Codon1.Character = as.character(Codon1)
-      Codon2.Character = as.character(Codon2)
-      #if  (grepl('-',Codon1.Character) | grepl('-',Codon2.Character)) {Indel=Indel+1; break}
-      #if  (!grepl('-',Codon1.Character) & !grepl('-',Codon2.Character)) 
-      
-      #{ 
-      A1 = as.character(Biostrings::translate(Codon1, genetic.code=SGC1))
-      A2 = as.character(Biostrings::translate(Codon2, genetic.code=SGC1))
-      
-      Subs = paste(A1,'>',A2,sep = '')
-      
-      AminoSubs = c(AminoSubs, Subs)  
-      
-      
-      if (A1 == A2) {Synon <- Synon + 1} else {Non_Synon <- Non_Synon +1}
-      
-      SquareOfDiffInComposition =  (GranthamDistance[GranthamDistance$A == A1,]$Composition - GranthamDistance[GranthamDistance$A == A2,]$Composition)^2
-      # Composition <<- c(Composition,paste(SquareOfDiffInComposition)) 
-      
-      SquareOfDiffInPolarity =    (GranthamDistance[GranthamDistance$A == A1,]$Polarity - GranthamDistance[GranthamDistance$A == A2,]$Polarity)^2
-      # Polarity <<- c(Polarity,paste(SquareOfDiffInPolarity))
-      
-      SquareOfDiffInVolume   =    (GranthamDistance[GranthamDistance$A == A1,]$Volume  - GranthamDistance[GranthamDistance$A == A2,]$Volume)^2
-      # Volume <<- c(Volume,paste(SquareOfDiffInVolume))
-      
-      GranthamNew <- (SquareOfDiffInComposition + SquareOfDiffInPolarity + SquareOfDiffInVolume)^0.5
-      Grantham <<-c(Grantham,paste(GranthamNew))
-      
-      # Output=paste(Synon,Non_Synon,Grantham,sep=';')
-      # one_line = rbind(one_line, c(AminoSubs, Grantham))
-      
-      AminoGrantham = paste(Subs, ':', GranthamNew,';','Synon:', Synon,';','Non_Synon:', Non_Synon, sep='')
-      
-      VecOfDistances = c(VecOfDistances, AminoGrantham)
-      #} 
-    }else{N=N+1;break}
+      if (CodonSubst1 %in% Check & CodonSubst2 %in% Check ) {
+        
+        Codon1 <- DNAString(CodonSubst1)
+        Codon2  <- DNAString(CodonSubst2)
+        
+        Codon1.Character = as.character(Codon1)
+        Codon2.Character = as.character(Codon2)
+        #if  (grepl('-',Codon1.Character) | grepl('-',Codon2.Character)) {Indel=Indel+1; break}
+        #if  (!grepl('-',Codon1.Character) & !grepl('-',Codon2.Character)) 
+        
+        #{ 
+        A1 = as.character(Biostrings::translate(Codon1, genetic.code=SGC1))
+        A2 = as.character(Biostrings::translate(Codon2, genetic.code=SGC1))
+        
+        Subs = paste(A1,'>',A2,sep = '')
+        
+        AminoSubs = c(AminoSubs, Subs)  
+        
+        
+        if (A1 == A2) {Synon <- Synon + 1} else {Non_Synon <- Non_Synon +1}
+        
+        SquareOfDiffInComposition =  (GranthamDistance[GranthamDistance$A == A1,]$Composition - GranthamDistance[GranthamDistance$A == A2,]$Composition)^2
+        # Composition <<- c(Composition,paste(SquareOfDiffInComposition)) 
+        
+        SquareOfDiffInPolarity =    (GranthamDistance[GranthamDistance$A == A1,]$Polarity - GranthamDistance[GranthamDistance$A == A2,]$Polarity)^2
+        # Polarity <<- c(Polarity,paste(SquareOfDiffInPolarity))
+        
+        SquareOfDiffInVolume   =    (GranthamDistance[GranthamDistance$A == A1,]$Volume  - GranthamDistance[GranthamDistance$A == A2,]$Volume)^2
+        # Volume <<- c(Volume,paste(SquareOfDiffInVolume))
+        
+        GranthamNew <- (1.833*SquareOfDiffInComposition + 0.1018*SquareOfDiffInPolarity + 0.000399*SquareOfDiffInVolume)^0.5
+        Grantham <<-c(Grantham,paste(GranthamNew))
+        
+        # Output=paste(Synon,Non_Synon,Grantham,sep=';')
+        # one_line = rbind(one_line, c(AminoSubs, Grantham))
+        
+        AminoGrantham = paste(Subs, ':', GranthamNew,';','Synon:', Synon,';','Non_Synon:', Non_Synon, sep='')
+        
+        VecOfDistances = c(VecOfDistances, AminoGrantham)
+        #} 
+      }else{N=N+1;break}
+    }
+    return(VecOfDistances)
   }
-  return(VecOfDistances)
-}
-
+  
 Codons$Distances = lapply(as.character(Codons$SubstVec), Translation)
+
+for(i in 1:nrow(Codons)){
+  Codons[i, 'NewDistance'] = paste(unlist(Codons$Distance[i]), collapse = ',')
+}
+  
+newCodons = Codons[, -7]
+write.table(newCodons,file = "/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Granyham_with_W.csv",quote = F, row.names = FALSE,
+              sep = '\t')
+
--- Head/2Scripts/03.CorrelationWithEcology.R
+++ Head/2Scripts/03.CorrelationWithEcology.R
@@ -1,7 +1,7 @@
 rm(list=ls(all=TRUE))
 
 Grantham = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Grantham.csv", sep='\t', header = TRUE) 
-#Grantham = Grantham1[1:10,]
+#Grantham = Grantham1[1:5000,]
 
     string.counter<-function(strings, pattern){  
   counts<-NULL
@@ -67,8 +67,8 @@ Grantham = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/G
  }
    
 for(i in 1:nrow(Grantham)){
-  Grantham$FractionOfSyn[i] =  as.numeric(Grantham$All_Synon[i])/ as.numeric(Grantham$TotalDiv)
-  Grantham$FractionOfNonsyn[i] =  as.numeric(Grantham$All_Non_Synon[i])/ as.numeric(Grantham$TotalDiv)
+  Grantham$FractionOfSyn[i] =  as.numeric(Grantham$All_Synon[i])/ as.numeric(Grantham$TotalDiv[i])
+  Grantham$FractionOfNonsyn[i] =  as.numeric(Grantham$All_Non_Synon[i])/ as.numeric(Grantham$TotalDiv[i])
     }
 ###################################################
           for(i in 1:nrow(Grantham)){
@@ -83,87 +83,75 @@ for(i in 1:nrow(Grantham)){
         SummOfAllGrantham = (SummOfAllGrantham + num)
         
       }
-      Grantham[i, "SummOfAllGrantham"] <- paste((as.numeric(SummOfAllGrantham)), collapse=";") 
+      Grantham[i,"SummOfAllGrantham"] <- paste((as.numeric(SummOfAllGrantham)), collapse=";") 
       Grantham[i,"AverageGrantham"]<- paste(as.numeric(SummOfAllGrantham)/as.numeric(Grantham$All_Non_Synon[i]))
     }
     }
 
-KnKs <-aggregate(as.numeric(Grantham$KnKs),list(Grantham$Species),mean,na.rm=TRUE)
+KnKs <-aggregate(as.numeric(Grantham$KnKs),list(Grantham$Species),median,na.rm=TRUE)
 colnames(KnKs) <- c("Species","KnKs")
 
-SummOfAllGrantham <- aggregate(as.numeric(Grantham$SummOfAllGrantham),list(Grantham$Species),mean,na.rm=TRUE)
+SummOfAllGrantham <- aggregate(as.numeric(Grantham$SummOfAllGrantham),list(Grantham$Species),median,na.rm=TRUE)
 colnames(SummOfAllGrantham) <- c("Species","SummOfAllGrantham")
 
-AverageGrantham <- aggregate(as.numeric(Grantham$AverageGrantham),list(Grantham$Species),mean,na.rm=TRUE)
+AverageGrantham <- aggregate(as.numeric(Grantham$AverageGrantham),list(Grantham$Species),median,na.rm=TRUE)
 colnames(AverageGrantham) <- c("Species","AverageGrantham")
 
-#Distance_for_each_species$Species = sub("_", " ", data$Species, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE)
+MedianOfAllSyn <- (aggregate((as.numeric(Grantham$All_Synon)),list(Grantham$Species),median,na.rm = TRUE))
+colnames(MedianOfAllSyn) <- c("Species","MedianOfAllSyn")
+MedianOfAllNonsyn <- (aggregate((as.numeric(Grantham$All_Non_Synon)),list(Grantham$Species),median,na.rm = TRUE))
+colnames(MedianOfAllNonsyn) <- c("Species","MedianOfAllNonsyn")
 
-df <- merge(AverageGrantham,
-            SummOfAllGrantham,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
-
-df1 <- merge(df,
-            KnKs,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
-
-
-
-
-
-MeanOfAllSyn <- (aggregate((as.numeric(Grantham$All_Synon)),list(Grantham$Species),mean,na.rm = TRUE))
-colnames(MeanOfAllSyn) <- c("Species","MeanOfAllSyn")
-MeanOfAllNonsyn <- (aggregate((as.numeric(Grantham$All_Non_Synon)),list(Grantham$Species),mean,na.rm = TRUE))
-colnames(MeanOfAllNonsyn) <- c("Species","MeanOfAllNonsyn")
-
-df2 <- merge(MeanOfAllSyn,MeanOfAllNonsyn,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
-Data1 <-merge(df1,df2,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
-
-FractionOfSyn <-aggregate(as.numeric(Grantham$FractionOfSyn),list(Grantham$Species),mean,na.rm=TRUE)
+FractionOfSyn <-aggregate(as.numeric(Grantham$FractionOfSyn),list(Grantham$Species),median,na.rm=TRUE)
 colnames(FractionOfSyn) <- c("Species","FractionOfSyn")
-
-FractionOfNonsyn <-aggregate(as.numeric(Grantham$FractionOfNonsyn),list(Grantham$Species),mean,na.rm=TRUE)
+FractionOfNonsyn <-aggregate(as.numeric(Grantham$FractionOfNonsyn),list(Grantham$Species),median,na.rm=TRUE)
 colnames(FractionOfNonsyn) <- c("Species","FractionOfNonsyn")
 
-Data2 <-merge(FractionOfSyn,FractionOfNonsyn,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
 
-Data <-merge(Data1,Data2,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
+df <- merge(AverageGrantham, SummOfAllGrantham,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
 
-write.table(Data,file = "/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Distances_KnKs.csv",quote = F, row.names = FALSE,sep = '\t')
+df1 <- merge(df,KnKs,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
 
+df2 <- merge(MedianOfAllSyn,MedianOfAllNonsyn,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
+
+df3 <- merge(FractionOfSyn,FractionOfNonsyn,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
+
+df4 <- merge(df1,df2,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
+
+df5 <- merge(df4,df3,by.x = "Species", by.y = "Species",all = FALSE,no.dups = TRUE,)
+
+write.table(df5,file = "/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Distances_KnKs_with_W.csv",quote = F, row.names = FALSE,sep = '\t')
 
 
 library(ape)
 library(gdata)
 library(ggplot2)
 
-data = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Distances_KnKs.csv", sep='\t', header = TRUE) 
-GenLength <- read.xls("/home/anastasia/mtdna-mammalian-evolution/Body/1Raw/GenerationLengthForMammals.xlsx")# табличка с продолжительностью жизни от Алины
+Data = read.table("/home/anastasia/mtdna-mammalian-evolution/Body/2Derived/Distances_KnKs_with_W.csv", sep='\t', header = TRUE) 
+GenLength<- read.xls("/home/anastasia/mtdna-mammalian-evolution/Body/1Raw/GenerationLengthForMammals.xlsx")# табличка с продолжительностью жизни от Алины
+#GenLength <- GenLength1[1:1,]
+Data$Species = sub("_", " ", Data$Species, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE)
 
-data$Species = sub("_", " ", data$Species, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE)
-
-Data = merge(data,GenLength, by.x = "Species", by.y = "Scientific_name",all = FALSE,no.dups = TRUE,)
+Data = merge(Data,GenLength, by.x = "Species", by.y = "Scientific_name",all = FALSE,no.dups = TRUE,)
 #Data <- NewData[,-13:-19]
 #Data <- Data[,-14]
 #Data <- Data[,-8]
 
 #GenerationLength_Distances
 
-cor.test(x = Data$GenerationLength_d, y = Data$Distance, method = "spearm")
-GenerationLength_Distances_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$Distance)
+cor.test(x = Data$GenerationLength_d, y = Data$SummOfAllGrantham , method = "spearm")
+GenerationLength_SummOfAllGrantham_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$SummOfAllGrantham )
 
-plot(x = Data$GenerationLength_d, y = Data$Distance)
-
-ggplot(Data, aes(x = log(GenerationLength_d), y = Distance,col = factor(Order) ))+
+ggplot(Data, aes(x = log(GenerationLength_d), y = SummOfAllGrantham ,col = factor(Order) ))+
   geom_point(size = 2)
 
-ggplot(Data, aes(x = log(GenerationLength_d), y = Distance, fill = Order)) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = SummOfAllGrantham , fill = Order)) + 
   geom_boxplot()
 
 #GenerationLength_KnKs
 
 cor.test(x = Data$GenerationLength_d, y = Data$KnKs, method = "spearm")
 GenerationLength_KnKs_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$KnKs)
-
-plot(x = Data$GenerationLength_d, y = Data$KnKs)
 
 ggplot(Data, aes(x = log(GenerationLength_d), y = KnKs)) + 
   geom_point() +
@@ -177,36 +165,32 @@ ggplot(Data, aes(x = log(GenerationLength_d), y = KnKs, fill = Order)) +
 
 #GenerationLength_proportion_of_S
 
-cor.test(x = Data$GenerationLength_d, y = Data$proportion_of_S, method = "spearm")
-GenerationLength_proportion_of_S_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$proportion_of_S)
+cor.test(x = Data$GenerationLength_d, y = Data$FractionOfSyn, method = "spearm")
+GenerationLength_FractionOfSyn_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$FractionOfSyn)
 
-plot(x = Data$GenerationLength_d, y = Data$proportion_of_S)
-
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_S)) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfSyn)) + 
   geom_point() +
   geom_smooth()
 
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_S, col = factor(Order))) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfSyn, col = factor(Order))) + 
   geom_point()
 
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_S, fill = Order)) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfSyn, fill = Order)) + 
   geom_boxplot()
 
 #GenerationLength_proportion_of_NS
 
-cor.test(x = Data$GenerationLength_d, y = Data$proportion_of_NS, method = "spearm")
-GenerationLength_proportion_of_NS_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$proportion_of_NS)
+cor.test(x = Data$GenerationLength_d, y = Data$FractionOfNonsyn, method = "spearm")
+GenerationLength_FractionOfNonsyn_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$FractionOfNonsyn)
 
-plot(x = Data$GenerationLength_d, y = Data$proportion_of_NS)
-
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_NS)) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfNonsyn)) + 
   geom_point() +
   geom_smooth()
 
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_NS, col = factor(Order))) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfNonsyn, col = factor(Order))) + 
   geom_point()
 
-ggplot(Data, aes(x = log(GenerationLength_d), y = proportion_of_NS, fill = Order)) + 
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfNonsyn, fill = Order)) + 
   geom_boxplot()
 
 #GenerationLength and AverageGrantham
@@ -226,5 +210,113 @@ ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGrantham, col = factor(
 ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGrantham, fill = Order)) + 
   geom_boxplot()
 
+#SummOfAllNonsyn ~ +GenerLength 
+
+cor.test(x = Data$GenerationLength_d, y = Data$AverageGrantham, method = "spearm", na.action = "na.exclude")
+GenerationLengthAverageGrantham_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$AverageGrantham)
+
+plot(x = Data$GenerationLength_d, y = Data$AverageGrantham)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGrantham)) + 
+  geom_point() +
+  geom_smooth()
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGrantham, col = factor(Order))) + 
+  geom_point()
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGrantham, fill = Order)) + 
+  geom_boxplot()
 
 
+
+
+
+
+
+
+
+
+
+
+#(i) FractionOfSyn ~ -GenerLength; 
+
+cor.test(x = Data$GenerationLength_d, y = Data$FractionOfSyn , method = "spearm")
+GenerationLength_FractionOfSyn_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$FractionOfSyn)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfSyn ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfSyn , fill = Order)) + 
+  geom_boxplot()
+
+
+#(ii) FractionOfNonsyn ~ +GenerLength; 
+
+cor.test(x = Data$GenerationLength_d, y = Data$FractionOfNonsyn , method = "spearm")
+GenerationLength_FractionOfNonsyn_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$FractionOfNonsyn)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfNonsyn ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = FractionOfNonsyn , fill = Order)) + 
+  geom_boxplot()
+
+#(iii) SummOfAllGrantham ~ +GenerLength; 
+cor.test(x = Data$GenerationLength_d, y = Data$SummOfAllGrantham , method = "spearm")
+GenerationLength_SummOfAllGrantham_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$SummOfAllGrantham)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = SummOfAllGrantham ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = SummOfAllGrantham , fill = Order)) + 
+  geom_boxplot()
+
+#(iv) SummOfAllNonsyn ~ +GenerLength 
+cor.test(x = Data$GenerationLength_d, y = Data$MeanOfAllNonsyn , method = "spearm")
+GenerationLength_MeanOfAllNonsyn_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$MeanOfAllNonsyn)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = MeanOfAllNonsyn ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y =MeanOfAllNonsyn , fill = Order)) + 
+  geom_boxplot()
+
+#(v) SummOfAllGrantham ~+SummOfAllNonsyn (проверка на вшивость)
+cor.test(x = Data$SummOfAllGrantham, y = Data$MeanOfAllNonsyn, method = "spearm")
+GenerationLength_MeanOfAllNonsyn_fit  <- cor.test(x = Data$SummOfAllGrantham, y = Data$MeanOfAllNonsyn)
+
+ggplot(Data, aes(x = log(SummOfAllGrantham), y = MeanOfAllNonsyn ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(SummOfAllGrantham), y = MeanOfAllNonsyn , fill = Order)) + 
+  geom_boxplot()
+
+#(vi) AverageGranthamN ~ +GenerLength; 
+cor.test(x = Data$GenerationLength_d, y = Data$AverageGranthamN , method = "spearm")
+GenerationLength_AverageGranthamN_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$AverageGranthamN)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGranthamN ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGranthamN, fill = Order)) + 
+  geom_boxplot()
+
+#(vii) AverageGranthamNS ~ +GenerLength; 
+cor.test(x = Data$GenerationLength_d, y = Data$AverageGranthamNS , method = "spearm")
+GenerationLength_AverageGranthamNS_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$AverageGranthamNS)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGranthamNS ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = AverageGranthamNS, fill = Order)) + 
+  geom_boxplot()
+
+#(vii) KnKs ~ +GenerLength; 
+cor.test(x = Data$GenerationLength_d, y = Data$KnKs , method = "spearm")
+GenerationLength_AverageGranthamNS_fit  <- cor.test(x = Data$GenerationLength_d, y = Data$KnKs)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = KnKs ,col = factor(Order) ))+
+  geom_point(size = 2)
+
+ggplot(Data, aes(x = log(GenerationLength_d), y = KnKs, fill = Order)) + 
+  geom_boxplot()
