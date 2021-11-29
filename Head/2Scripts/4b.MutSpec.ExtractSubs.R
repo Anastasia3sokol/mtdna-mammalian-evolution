de0a3c4291964c8d789267feb2b097faa444d4a8
Dmitrii Iliushchenko <43323318+DIliushchenko@users.noreply.github.com>
2021-11-29 17:42:20 +0200

    bug fixes

--- Head/2Scripts/4b.MutSpec.ExtractSubs.R
+++ Head/2Scripts/4b.MutSpec.ExtractSubs.R
@@ -1,6 +1,6 @@
 rm(list=ls(all=TRUE))
 
-mut = read.table('../../Body/2Derived/MutSpec.DerivedSubs.txt')
+mut = read.csv('../../Body/2Derived/MutSpec.DerivedSubs.csv')
 mut$MutSpec = as.character(mut$MutSpec)
 
 new_mut = data.frame()
@@ -10,9 +10,9 @@ new_mut = data.frame()
 for ( i in 1:nrow(mut))
 {#i = 1
   subs = unlist(strsplit(mut$MutSpec[i],';'))
-  for (c in 2:length(lenn))
+  for (c in 2:length(subs))
   { #c = 2
-    sub = unlist(strsplit(lenn[c],'\\.'))
+    sub = unlist(strsplit(susb[c],'\\.'))
     df = data.frame(mut$Outgroup[i],mut$Ingroup0[i],mut$Ingroup1[i],sub[1],sub[2],sub[3])
     new_mut = rbind(new_mut, df)
   }
